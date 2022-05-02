d3eba6be833c2e43790f657fec2874fdcb987095
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-05-02 20:42:56 +0200

    Implement moving to add phone activity

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.lab3;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private PhoneViewModel mPhoneViewModel;
     private PhoneListAdapter mAdapter;
 
     RecyclerView recyclerView;
+    private FloatingActionButton mFabAdd;
+    private ActivityResultLauncher<Intent> mActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +33,22 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new PhoneListAdapter(this);
         recyclerView.setAdapter(mAdapter);
 
+        mFabAdd = findViewById(R.id.fabAdd);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mPhoneViewModel = new ViewModelProvider(this).get(PhoneViewModel.class);
 
         mPhoneViewModel.getAllPhones().observe(this, phones -> mAdapter.setPhoneList(phones));
+
+        mFabAdd.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddPhoneActivity.class);
+            mActivityResultLauncher.launch(intent);
+        });
+
+        mActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {}
+        );
     }
 
     @Override
